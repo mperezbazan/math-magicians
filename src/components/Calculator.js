34f157ac8ca60f1f598b058c0711c6ac2eb4123a
Martin Perez Bazan <martin.perezbazan@gmail.com>
2022-12-01 12:48:26 -0300

    Add logic to buttons

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import calculate from '../logic/calculate';
 import Button from './Button';
 import Result from './Result';
 
 const initialButtons = [
   { value: 'AC' },
-  { value: '±' },
+  { value: '+/-' },
   { value: '%' },
   { value: '÷', color: 'orange' },
   { value: '7' },
@@ -26,20 +27,52 @@ const initialButtons = [
 class Calculator extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { buttons: initialButtons };
+    this.state = {
+      buttons: initialButtons,
+      total: '0',
+      next: null,
+      operation: null,
+      screen: '0',
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(value) {
+    const newData = calculate(this.state, value);
+    this.setState(newData);
+    if ((newData.operation && newData.total) || newData.operation === undefined) {
+      this.setState({ screen: newData.next });
+    }
+    if (newData.next === null) {
+      this.setState({ screen: '0' });
+    }
+    if (newData.total !== null && newData.next === null) {
+      this.setState({ screen: newData.total });
+    }
+    if (!newData.total && !newData.next && !newData.operation) {
+      this.setState({ screen: '0', next: '0' });
+    }
+    if (value === '+/-') {
+      if (newData.total !== null) {
+        this.setState({ screen: newData.total });
+      } else {
+        this.setState({ screen: newData.next });
+      }
+    }
   }
 
   render() {
-    const { buttons } = this.state;
+    const { buttons, screen } = this.state;
     return (
       <div className="calculator-container">
-        <Result value={0} />
+        <Result value={screen} />
         {buttons.map((button) => (
           <Button
             color={button.color}
             value={button.value}
             columns={button.columns}
             key={button.value}
+            handleClick={this.handleClick}
           />
         ))}
       </div>
